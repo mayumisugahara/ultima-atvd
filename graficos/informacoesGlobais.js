36a898224e73e054aa8735df5fd353428736c178
mayumisugahara <106741982+mayumisugahara@users.noreply.github.com>
2024-11-19 15:28:47 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -11,7 +11,7 @@ async function vizualizarInformacoesGlobais() {
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
-    paragrafo.innerHTML = `Você sabia que o mundo tem <span>${pessoasNoMundo} bilhões</span> de pessoas e que aproximadamente <span>${pessoasConectadas} bilhões</span> estão conectadas em alguma rede social e passam em média <span>${horas} horas</span> e <span>${minutos} minutos</span> conectadas.<br>Isso significa que aproximadamente <span>${porcentagemConectada}%</span> de pessoas estão conectadas em alguma rede social.`
+    paragrafo.innerHTML = `vc sabia que a maioria dos estudantes preferem ficar em aplicativos do celular ao invés de interagir com pessoas.`
 
     const container = document.getElementById('graficos-container')
     container.appendChild(paragrafo)
